e98b4eae7fbe1ed1ecad4a32827445305226184c
bharti mali <malibharti5@gmail.com>
2020-10-20 21:24:58 +0530

    MODIFY : Operations performed on inputs

--- sorting.sh
+++ sorting.sh
@@ -3,4 +3,5 @@ read -p "Enter the first input - " a
 read -p "Enter the second input - " b
 read -p "Enter the third input - " c
 echo "Entered inputs are - " $a $b $c
- 
+result=$((a+b*c)) 
+echo "Result - " $result
